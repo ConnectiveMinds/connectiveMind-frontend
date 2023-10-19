405e267212c87ea0f6f63b1c4bb5fd67382bea88
Jenisha Khulal <jenishakhulal@gmail.com>
2023-10-19 10:06:13 +0545

    improvisation on landing page

--- src/Components/Description.tsx
+++ src/Components/Description.tsx
@@ -2,19 +2,19 @@ import React from "react";
 
 export function Description() {
   return (
-    <div className="body">
-      <div className="intro">
+    <div className="description">
+      <div className="intro" >
         <header>
           Meet<span>ConnectiveMinds</span>{" "}
         </header>
-        <div>
+        <div className="about">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
           reprehenderit sit beatae ducimus, consectetur dignissimos nisi. Ipsum
           culpa delectus magni in quisquam quia ut sed vel blanditiis fugit!
           Temporibus, iure.
         </div>
       </div>
-      <div>
+      <div className="images">
         <img className="bulb-bg" src="src\Data\Ellipse 1.png" alt="" />
         <img
           className="bulb"
