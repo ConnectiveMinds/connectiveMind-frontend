9469bd8a0410562dc61bbf624c0e60905b752d92
Sudip8486 <sudeepbhattarai1792@gmail.com>
2023-11-28 22:51:01 +0545

    Chat & get idea by idea implemented

--- src/services/homepageServices.ts
+++ src/services/homepageServices.ts
@@ -1,9 +1,18 @@
-import axios from 'axios';
-import { creategroup, ownerId, AuthToken } from '../utils/apiroutes';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import axios from "axios";
+import {
+  creategroup,
+  ownerId,
+  AuthToken,
+  getchat,
+  getGroupsByUserId,
+} from "../utils/apiroutes";
 
-
-
-export const createGroup = async (teamName: string, projectDescription: string, skillsRequired: string) => {
+export const createGroup = async (
+  teamName: string,
+  projectDescription: string,
+  skillsRequired: string
+) => {
   try {
     const response = await axios.post(
       creategroup,
@@ -12,12 +21,12 @@ export const createGroup = async (teamName: string, projectDescription: string, 
         title: teamName,
         description: projectDescription,
         skills: [skillsRequired],
-        status: 'open',
+        status: "open",
       },
       {
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer '+AuthToken,
+          "Content-Type": "application/json",
+          Authorization: AuthToken,
         },
       }
     );
@@ -28,3 +37,32 @@ export const createGroup = async (teamName: string, projectDescription: string, 
   }
 };
 
+export const getmessages = async (projectId: string) => {
+  try {
+    const url = getchat + projectId;
+    const response = await axios.get(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: AuthToken,
+      },
+    });
+    return response.data;
+  } catch (error: any) {
+    throw new Error(`Error: ${error.message}`);
+  }
+};
+
+export const getIdeaByUserId = async () => {
+  try {
+    const response = await axios.get(getGroupsByUserId, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: AuthToken,
+      },
+    });
+    return response.data;
+  } catch (e: any) {
+    throw new Error(`Error: ${e.message}`);
+    /* empty */
+  }
+};
--- src/utils/apiroutes.tsx
+++ src/utils/apiroutes.tsx
@@ -1,4 +1,8 @@
 export const host = "http://localhost:3000";
-export const ownerId= '64eb17f7fd2129889d14983d';
-export const creategroup= host+"/api/v1/idea/create";
-export const AuthToken= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGViMTdmN2ZkMjEyOTg4OWQxNDk4M2QiLCJlbWFpbCI6InN1ZGVlcGJoYXR0YXJhaTE3OTJAZ21haWwuY29tIiwiaWF0IjoxNzAwNzM4NDk3LCJleHAiOjE3MDA4MjQ4OTd9.H3AyTbR8p_IS_GiLUuyPstCoYNyM8nQNcMerXCjZvX8";
+export const ownerId = "64eb17f7fd2129889d14983d";
+
+export const creategroup = host + "/api/v1/idea/create/";
+export const getGroupsByUserId = `${host}/api/v1/idea/get/`;
+export const getchat = `${host}/api/v1/chat/get/`;
+export const AuthToken =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGViMTdmN2ZkMjEyOTg4OWQxNDk4M2QiLCJlbWFpbCI6InN1ZGVlcGJoYXR0YXJhaTE3OTJAZ21haWwuY29tIiwiaWF0IjoxNzAxMTg1NjEzLCJleHAiOjE3MDEyNzIwMTN9.RU20yEjqda7Sc2DTHm8mo-8VJlSJg4W9jB6k0ugsL44";
