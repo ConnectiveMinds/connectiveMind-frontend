64ef6612083e46d76eb0a1f09539d536e6d267d9
Sudip8486 <sudeepbhattarai1792@gmail.com>
2023-12-01 21:17:52 +0545

    Join Button Implemneted

--- src/Components/Button/customButton.tsx
+++ src/Components/Button/customButton.tsx
@@ -1,14 +1,17 @@
+import { ReactEventHandler } from "react";
+
 interface IButton {
   text: string;
   height: number;
   width: number;
+  onClick?: ReactEventHandler;
 }
 export function CustomButton(props: IButton) {
   return (
     <div>
       <button
         className={`text-purple-800  hover:bg-purple-800 hover:text-white text-xs h-${props.height} w-${props.width}  bg-white rounded-[10px] border-2 border-purple-800`}
-        onClick={() => {}}
+        onClick={props.onClick}
       >
         {props.text}
       </button>
--- src/Components/Cards/projects_card.tsx
+++ src/Components/Cards/projects_card.tsx
@@ -1,27 +1,50 @@
+import { useEffect } from "react";
+import { updatejoinRequest } from "../../services/homepageServices";
 import { CustomButton } from "../Button/customButton";
-import { ISkillCard, SkillCard } from "./skills_card";
+import { SkillCard } from "./skills_card";
+import { io } from "socket.io-client";
 
 export interface IProjectCard {
-  name: string;
+  title: string;
+  _id: string;
+  ownerId: string;
   description: string;
-  skills: ISkillCard[];
+  skills: string[];
 }
 export function ProjectCard(props: IProjectCard) {
+  const socket = io("http://localhost:3000");
+  const handleClick = async () => {
+    {
+      updatejoinRequest(props._id).then(async (data) => {
+        await socket.emit("join_room", props.ownerId);
+        await socket.emit("send_request", data["data"]);
+      });
+    }
+  };
+  useEffect(() => {
+    socket.on("receive_request", (data) => {});
+  }, [socket]);
   const skilllist = props.skills.map((skill) => {
     return (
       <li>
-        <SkillCard name={skill.name}></SkillCard>
+        <SkillCard name={skill}></SkillCard>
       </li>
     );
   });
+  console.log(skilllist);
   return (
     <div className="max-w-fit max-h-fit bg-white rounded-[10px] border-2 border-zinc-300">
       <div className="p-4">
         <div className="flex flex-row justify-between items-center">
           <div className="w-32 h-7 text-black text-2xl font-normal font-['Inria Serif'] tracking-tight">
-            {props.name}
+            {props.title}
           </div>
-          <CustomButton text="Join" height={8} width={32}></CustomButton>
+          <CustomButton
+            text="Join"
+            height={8}
+            width={32}
+            onClick={handleClick}
+          ></CustomButton>
         </div>
         <div className="max-w-full max-h-fit bg-gray-200 rounded-lg relative mt-4 p-4 text-black text-base font-normal font-['Inria Serif'] tracking-tight">
           <p>{props.description}</p>
