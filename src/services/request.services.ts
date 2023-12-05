ec8c3f5741e83bcf0434ac19fdf35376cd7ff7a1
Sudip8486 <sudeepbhattarai1792@gmail.com>
2023-12-05 09:30:46 +0545

    Minor Changes

--- src/services/request.services.ts
+++ src/services/request.services.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { AuthToken, incomingRequest } from "../utils/apiroutes";
+import { AuthToken, incomingRequest, sentRequest } from "../utils/apiroutes";
 
 export const getIncomingRequest = async () => {
   try {
@@ -10,6 +10,21 @@ export const getIncomingRequest = async () => {
       },
     });
 
+    return response.data;
+  } catch (e: any) {
+    throw new Error(`Error: ${e.message}`);
+    /* empty */
+  }
+};
+
+export const getSentRequset = async () => {
+  try {
+    const response = await axios.get(sentRequest, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: AuthToken,
+      },
+    });
     return response.data;
   } catch (e: any) {
     throw new Error(`Error: ${e.message}`);
--- src/utils/apiroutes.tsx
+++ src/utils/apiroutes.tsx
@@ -5,8 +5,9 @@ export const group = `${host}/api/v1/idea/crud/`;
 export const getGroupsByUserId = `${host}/api/v1/idea/get/`;
 export const getallgroups = `${host}/api/v1/idea/get/all`;
 export const incomingRequest = `${host}/api/v1/idea/get/joinrequest`;
+export const sentRequest = `${host}/api/v1/idea/get/sentrequest`;
 
 export const getchat = `${host}/api/v1/chat/get/`;
 export const userSignUp = `${host}/api/v1/user/auth/signup`;
 export const AuthToken =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTY5ZWI4YWYxMjQxMzQ4ODRlMjNjYjEiLCJlbWFpbCI6InN1ZGVlcGJoYXR0YXJhaTE3OTJAZ21haWwuY29tIiwiaWF0IjoxNzAxNDQ0NDE3LCJleHAiOjE3MDE1MzA4MTd9.DDa72CLxmfWmsEI-zgyMQPzBH5JxZJ1AAqlT44BHMf8";
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTY5ZWI4YWYxMjQxMzQ4ODRlMjNjYjEiLCJlbWFpbCI6InN1ZGVlcGJoYXR0YXJhaTE3OTJAZ21haWwuY29tIiwiaWF0IjoxNzAxNzQ3NTgyLCJleHAiOjE3MDE4MzM5ODJ9.Klq66b7VvGmQ1wwdNBPWTTOZ2MQNbhTryXa9515Jwbs";
