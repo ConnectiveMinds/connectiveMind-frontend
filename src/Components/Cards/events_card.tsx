dab8758cff5c6d1f1d0b49ce52c88d79bb133cb4
Sudip8486 <sudeepbhattarai1792@gmail.com>
2023-12-13 22:31:33 +0545

    Upcoming Events Completed

---
+++ src/Components/Cards/events_card.tsx
@@ -0,0 +1,15 @@
+export interface IEventCard {
+  _id: string;
+  title: string;
+  time: string;
+}
+export function EventCard(props: IEventCard) {
+  return (
+    <div className="  relative h-12 bg-zinc-300 rounded-lg mr-4 mt-4 flex flex-row justify-between text-black text-xl font-normal font-['Inter']">
+      <p className="ml-2 mt-2 overflow-x-clip overflow-y-clip">{props.title}</p>
+      <p className="ml-4 mt-2 mr-4 overflow-x-clip overflow-y-clip">
+        In:{props.time}
+      </p>
+    </div>
+  );
+}
--- src/features/HomePage/components/eventssection.tsx
+++ src/features/HomePage/components/eventssection.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { IEventCard } from "../../../Components/Cards/events_card";
+import { UpcomingEvents } from "./upcomingeEvents";
+
 export function Events() {
+  const [upcomingEventList, setUpcomningEventList] = useState<
+    Array<IEventCard>
+  >([]);
+
+  useEffect(() => {
+    setUpcomningEventList([]);
+  }, []);
+
   return (
     <div className="max-w-full">
-      <div className="w-72 h-64 relative"></div>
+      <div className="w-96 flex flex-col justify-end">
+        <UpcomingEvents events={upcomingEventList} />
+
+        <UpcomingEvents events={upcomingEventList} />
+        <div className="mt-auto"></div>
+      </div>
     </div>
   );
 }
